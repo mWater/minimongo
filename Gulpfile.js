996fff2e159bc73f69b6bf631626122c340630ae
Clayton Grassick <clayton@claytronics.org>
2014-05-12 13:30:15 -0400

    Improved build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var browserify = require('browserify');
+var streamConvert = require('vinyl-source-stream');
+var glob = require('glob');
+var coffeeify = require('coffeeify');
 
 // Compilation
 gulp.task('coffee', function() {
@@ -12,6 +16,15 @@ gulp.task('coffee', function() {
 gulp.task('copy', function() {
 	gulp.src(['./src/**/*.js'])
 		.pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('prepareTests', ['coffee', 'copy'], function() {
+	var bundler = browserify({entries: glob.sync("./test/*Tests.coffee"), extensions: [".coffee"] }).
+		transform(coffeeify);
+	var stream = bundler.bundle()
+		.pipe(streamConvert('browserified.js'))
+		.pipe(gulp.dest('./test'));
+	return stream;
 });
 
 gulp.task('default', ['coffee', 'copy']);
