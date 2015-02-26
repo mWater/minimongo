8a924dda49feb0e288c54be3bf6aa44118ce5852
Clayton Grassick <clayton@claytronics.org>
2015-02-26 13:42:40 -0500

    Race condition fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,13 @@ var rename = require("gulp-rename");
 
 // Compilation
 gulp.task('coffee', function() {
-	gulp.src('./src/*.coffee')
+	return gulp.src('./src/*.coffee')
 		.pipe(coffee({ bare: true }).on('error', gutil.log))
 		.pipe(gulp.dest('./lib/'));
 });
 
 gulp.task('copy', function() {
-	gulp.src(['./src/**/*.js'])
+	return gulp.src(['./src/**/*.js'])
 		.pipe(gulp.dest('./lib/'));
 });
 
