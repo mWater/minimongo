294aad0f3dd4cefbb5a95770661714370adb6d43
Clayton Grassick <clayton@claytronics.org>
2019-12-13 09:22:52 -0500

    Added typings

---
+++ index.d.ts
@@ -0,0 +1,74 @@
+export interface MinimongoCollectionFindOptions {
+  fields?: any
+  sort?: any
+  limit?: number
+  skip?: number
+  cacheFind?: boolean
+  interim?: boolean
+  timeout?: number
+
+  /** Only for RemoteDb.find */ 
+  localData?: any[]
+
+  /** Only for RemoteDb.find */ 
+  whereExpr?: Expr
+  /** Only for RemoteDb.find */ 
+  orderByExprs?: { expr: Expr, dir: "asc" | "desc" }[]
+}
+
+export class MinimongoDb {
+  addCollection<T>(name: string, options?: any, success?: (collection: MinimongoCollection<T>) => void, error?: (err: any) => void): void
+  removeCollection<T>(name: string, success?: () => void, error?: (err: any) => void): void
+  getCollectionNames(): string[]
+  localDb?: MinimongoDb
+  remoteDb?: MinimongoDb 
+  [collectionName: string]: MinimongoCollection<any> 
+}
+
+export interface MinimongoCollection<ItemType> {
+  find(selector: any, options?: MinimongoCollectionFindOptions): { 
+    fetch(success: (items: ItemType[]) => void, error: (err: any) => void): void
+  }
+
+  find(selector: any): { 
+    fetch(success: (items: ItemType[]) => void, error: (err: any) => void): void
+  }
+
+  findOne(selector: any, options: { fields?: any, sort?: any, limit?: number, skip?: number, interim?: boolean, cacheFindOne?: boolean, timeout?: number }, success: (item: ItemType | null) => void, error: (err: any) => void): void
+
+  findOne(selector: any, success: (item: ItemType | null) => void, error: (err: any) => void): void
+
+  upsert(item: ItemType, success: (item: ItemType | null) => void, error: (err: any) => void): void
+  upsert(item: ItemType, baseRow: ItemType, success: (item: ItemType | null) => void, error: (err: any) => void): void
+  upsert(items: ItemType[], success: (item: ItemType | null) => void, error: (err: any) => void): void
+  upsert(items: ItemType[], baseRows: ItemType[], success: (item: ItemType | null) => void, error: (err: any) => void): void
+
+  remove(itemId: string, success: () => void, error: (err: any) => void): void
+
+  cache?(docs: ItemType[], selector: any, options: any, success: () => void, error: (err: any) => void): void
+  pendingUpserts?(success: (items: ItemType[]) => void, error: (err: any) => void): void
+  pendingRemoves?(success: (ids: string[]) => void, error: (err: any) => void): void
+  resolveRemove?(id: string, success: () => void, error: (err: any) => void): void
+  /** Add but do not overwrite or record as upsert */
+  seed?(docs: ItemType[], success: () => void, error: (err: any) => void): void
+
+  /** Add but do not overwrite upserts or removes */
+  cacheOne?(doc: ItemType, success: () => void, error: (err: any) => void): void
+
+  /** Add but do not overwrite upserts or removes */
+  cacheList?(docs: ItemType[], success: () => void, error: (err: any) => void): void
+
+  uncache?(selector: any, success: () => void, error: (err: any) => void): void
+  uncacheList?(ids: string[], success: () => void, error: (err: any) => void): void
+}
+
+
+export class MemoryDb extends MinimongoDb {}
+export class LocalStorageDb extends MinimongoDb {}
+export class IndexedDb extends MinimongoDb {}
+export class WebSQLDb extends MinimongoDb {}
+export class RemoteDb extends MinimongoDb {}
+export class HybridDb extends MinimongoDb {}
+export class ReplicatingDb extends MinimongoDb {}
+// exports.quickfind = require('./lib/quickfind');
+// exports.utils = require('./lib/utils');
