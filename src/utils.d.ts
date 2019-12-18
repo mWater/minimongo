a0d76b6d5a448b67e04bd69d5336ca574926aa43
Clayton Grassick <clayton@claytronics.org>
2019-12-17 21:04:39 -0500

    Added declaration

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -1,3 +1,8 @@
+import { MinimongoCollection } from "..";
 
 /** Compile a document selector (query) to a lambda function */
 export function compileDocumentSelector(selector: any): (doc: any) => boolean 
+
+/** Clone a local database collection's caches, pending upserts and removes from one database to another
+ * Useful for making a replica */
+export function cloneLocalCollection(fromCol: MinimongoCollection, toCol: MinimongoCollection, success: () => void, error: (err: any) => void): void
